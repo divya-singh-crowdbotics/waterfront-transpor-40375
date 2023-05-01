e33bc4021cad09909bcf9492ebb95e6d4127fc28
Crowdbotics <team@crowdbotics.com>
2023-05-01 17:40:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-wave-40375",
+  url: "https://dry-wave-40375.botics.co",
+  api: "https://dry-wave-40375.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
