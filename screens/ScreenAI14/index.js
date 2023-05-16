fe938eaa1e39f06349d5cda713bc1d9ac9a9fe13
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-05-16 16:41:18 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/ScreenAI14/index.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+
+const JobsScreen = () => {
+  return <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={{
+        uri: 'https://tinyurl.com/42evm3m3'
+      }} style={styles.logo} />
+        <TouchableOpacity style={styles.notificationIcon}>
+          <Image source={{
+          uri: 'https://tinyurl.com/42evm3m3'
+        }} style={styles.notificationIconImage} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.searchBarContainer}>
+        <TextInput placeholder="Search Jobs" style={styles.searchBar} />
+        <TouchableOpacity style={styles.searchButton}>
+          <Text style={styles.searchButtonText}>Search</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.jobsListContainer}>
+        {
+        /* Dummy data for jobs list */
+      }
+        <Text style={styles.jobTitle}>Truck Driver</Text>
+        <Text style={styles.jobDescription}>We are looking for a truck driver with a valid CDL license and a clean driving record.</Text>
+        <Text style={styles.jobLocation}>Location: New York, NY</Text>
+        <Text style={styles.jobSalary}>Salary: $50,000 - $70,000 per year</Text>
+      </View>
+      <View style={styles.navigationMenu}>
+        <TouchableOpacity style={styles.navigationButton}>
+          <Text style={styles.navigationButtonText}>Home - Jobs</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navigationButton}>
+          <Text style={styles.navigationButtonText}>Map</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navigationButton}>
+          <Text style={styles.navigationButtonText}>Wallet</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navigationButton}>
+          <Text style={styles.navigationButtonText}>History</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navigationButton}>
+          <Text style={styles.navigationButtonText}>Settings</Text>
+        </TouchableOpacity>
+      </View>
+    </View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingTop: 40
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  logo: {
+    width: 100,
+    height: 50
+  },
+  notificationIcon: {
+    backgroundColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 50
+  },
+  notificationIconImage: {
+    width: 20,
+    height: 20
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20
+  },
+  searchBar: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 10,
+    marginRight: 10
+  },
+  searchButton: {
+    backgroundColor: '#1abc9c',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10
+  },
+  searchButtonText: {
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  jobsListContainer: {
+    marginTop: 20
+  },
+  jobTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  jobDescription: {
+    fontSize: 16,
+    marginBottom: 10
+  },
+  jobLocation: {
+    fontSize: 14,
+    color: '#999',
+    marginBottom: 5
+  },
+  jobSalary: {
+    fontSize: 14,
+    color: '#1abc9c',
+    fontWeight: 'bold'
+  },
+  navigationMenu: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#f2f2f2',
+    paddingVertical: 10,
+    paddingHorizontal: 20
+  },
+  navigationButton: {
+    backgroundColor: '#fff',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10
+  },
+  navigationButtonText: {
+    color: '#1abc9c',
+    fontWeight: 'bold'
+  }
+});
+export default JobsScreen;
