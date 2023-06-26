c682581c3d42d2e290dc3f7c2a6d9701cc09136c
Divya Singh <divya.singh@nagarro.com>
2023-06-26 14:13:21 +0530

    LogoutDone

--- backend/home/api/v1/urls.py
+++ backend/home/api/v1/urls.py
@@ -4,11 +4,13 @@ from rest_framework.routers import DefaultRouter
 from home.api.v1.viewsets import (
     SignupViewSet,
     LoginViewSet,
+    LogoutViewSet
 )
 
 router = DefaultRouter()
 router.register("signup", SignupViewSet, basename="signup")
 router.register("login", LoginViewSet, basename="login")
+router.register("user", LogoutViewSet, basename="user")
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -2,11 +2,16 @@ from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from django.contrib.auth import logout
+from rest_framework import serializers, status
+from rest_framework.decorators import action
+from django.contrib.auth import get_user_model
 
 from home.api.v1.serializers import (
     SignupSerializer,
     UserSerializer,
 )
+User = get_user_model()
 
 
 class SignupViewSet(ModelViewSet):
@@ -28,3 +33,36 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+    
+
+class LogoutViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(detail=False, methods=['post'])  
+    def logout(self,request):
+            try:
+                token = request.META.get('HTTP_AUTHORIZATION', '').split(' ')[-1]
+                try:
+                    token_obj = Token.objects.get(key=token)
+                except Token.DoesNotExist:
+                    return Response({'data': 'Log out failed' ,
+                                                'status':status.HTTP_400_BAD_REQUEST,
+                                                'message':'Token not exists'})
+                token_obj.delete()
+                try:
+                    logout(request)
+                except Exception as e:
+                    return Response({'data': e,
+                                            'status':status.HTTP_400_BAD_REQUEST,
+                                            'message':'Logout function Failed'})
+                return Response({'data': "User logged out",
+                                    'status':status.HTTP_200_OK,
+                                    'message':'User logged out'})
+
+              
+            except Exception as e:
+                return Response({'data': e,
+                                    'status':status.HTTP_400_BAD_REQUEST,
+                                    'message':'Logout Failed'}) 
+
